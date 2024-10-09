944b80cb9c9a7f8c3536a668e649670026808871
AndryV <andryvj2@gmail.com>
2024-10-09 14:33:30 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 
    [x] Pegar a informaçao do Input, quando o botão for clicado
    [x] Ir até a API, e trazer as receitas
-   [ ] Colocar as receitas na Tela
+   [x] Colocar as receitas na Tela
    [ ] Saber quando o usuario clicou na receita
    [ ] Buscar informaçoes da receita individual na API
    [ ]Colocar receita individual na tela
@@ -37,11 +37,31 @@ async function searchRecipes(ingredient) {
 
 function showRecipes(recipes) {
     recipelist.innerHTML = recipes.map(item => `
-        <div class="recipe-card">
+        <div class="recipe-card" onclick="getRecipesDetails(${item.idMeal})"    >
         <img src="${item.strMealThumb}" alt="receita-foto">
         <h3>${item.strMeal}</h3>
         </div>
         `
 
     ).join('')
+}
+
+async function getRecipesDetails(id){
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`,)
+    const data = await response.json()
+    const recipe = data.meals[0]
+    let ingredients = ''
+    
+    for(let i = 1; i <= 20; i++){
+        if(recipe[`strIngredient${i}`]){
+            ingredients = `<li>${}</li>` recipe[`strIngredient${i}
+
+            ]
+            
+            console.log (recipe[`strIngredient${i}`])   
+        }else{
+            break;      
+        }
+    }
+    
 }
